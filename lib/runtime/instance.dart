646078c9b95eb8552190eabc4fdb0617ddb56a4d
zyrouge <naveencnaveen96@gmail.com>
2022-01-27 13:42:36 +0530

    refactor: update runtime

--- lib/runtime/instance.dart
+++ lib/runtime/instance.dart
@@ -1,9 +1,15 @@
+import 'dart:typed_data';
 import 'package:hetu_script/hetu_script.dart';
 
 const String _extractorIdentifier = 'extractor';
 
 class ERuntimeInstance {
   final Hetu hetu = Hetu();
+
+  Future<void> loadByteCode(final Uint8List bytes) async {
+    // TODO: fix `moduleName`
+    hetu.loadBytecode(bytes: bytes, moduleName: '');
+  }
 
   Future<T> getExtractor<T>() async {
     final dynamic result = await hetu.eval(_extractorIdentifier);
