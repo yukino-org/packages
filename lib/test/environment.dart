7df3c7660c2d3659ea4e18f7d0f7cd03e98c1aa7
zyrouge <naveencnaveen96@gmail.com>
2022-01-28 11:38:24 +0530

    chore: log summary

--- lib/test/environment.dart
+++ lib/test/environment.dart
@@ -22,29 +22,50 @@ abstract class TEnvironment {
             : null;
 
     final List<String> methods = envMethods?.split(',') ?? tests.keys.toList();
+    final Map<String, bool> result = <String, bool>{};
 
-    for (final String method in methods) {
-      if (tests.containsKey(method)) {
+    for (final String x in methods) {
+      if (tests.containsKey(x)) {
         final TimeTracker time = TimeTracker()..start();
 
         try {
-          TConsole.p('Running: ${Colorize('$method()').cyan()}');
+          TConsole.p('Running: ${Colorize('$x()').cyan()}');
 
-          await tests[method]!();
+          await tests[x]!();
 
+          result[x] = true;
           TConsole.p(
-            'Passed: ${Colorize('$method()').cyan()} ${Colorize('(${time.elapsed}ms)').darkGray()}',
+            'Passed: ${Colorize('$x()').cyan()} ${Colorize('(${time.elapsed}ms)').darkGray()}',
           );
         } catch (err, stack) {
+          result[x] = false;
           TConsole.err(err, stack);
           TConsole.p(
-            'Failed: ${Colorize('$method()').cyan()} ${Colorize('(${time.elapsed}ms)').darkGray()}',
+            'Failed: ${Colorize('$x()').cyan()} ${Colorize('(${time.elapsed}ms)').darkGray()}',
           );
         }
 
         TConsole.ln();
       }
     }
+
+    final int passed =
+        result.values.fold(0, (final int pv, final bool x) => x ? pv + 1 : pv);
+    final int failed = result.length - passed;
+
+    TConsole.p(
+      'Summary: (${Colorize('$passed').green()} ${Colorize('$failed').red()})',
+    );
+
+    TConsole.p(
+      result.entries
+          .map(
+            (final MapEntry<String, bool> x) => x.value
+                ? '${Colorize('$x()').cyan()}: ${Colorize('✓').green()}'
+                : '${Colorize('$x()').cyan()}: ${Colorize('⨉').red()}',
+          )
+          .join('\n'),
+    );
   }
 
   static Future<void> dispose() async {
