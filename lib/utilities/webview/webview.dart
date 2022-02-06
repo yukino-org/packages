aa764d1702ca57dfdba7dc8667d599ffd3f33bf0
zyrouge <naveencnaveen96@gmail.com>
2022-02-06 13:00:36 +0530

    refactor: webview manager options

--- lib/utilities/webview/webview.dart
+++ lib/utilities/webview/webview.dart
@@ -4,18 +4,25 @@ import './models/provider.dart';
 export './models/provider.dart';
 export './models/webview.dart';
 
+class WebviewManagerInitializeOptions {
+  const WebviewManagerInitializeOptions(this.provider, this.options);
+
+  final WebviewProvider<dynamic> provider;
+  final WebviewProviderOptions options;
+}
+
 abstract class WebviewManager {
   static bool ready = false;
 
   static late final WebviewProvider<dynamic> provider;
 
-  static Future<void> initialize<T extends WebviewProvider<T>>(
-    final WebviewProvider<T> provider,
-    final WebviewProviderOptions options,
+  static Future<void> initialize(
+    final WebviewManagerInitializeOptions options,
   ) async {
     if (ready) throw Exception('Already in use');
 
-    await provider.initialize(options);
+    provider = options.provider;
+    await provider.initialize(options.options);
 
     ready = true;
   }
