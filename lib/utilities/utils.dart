6bcf446cda043e70a46cdd8f8ae56312eafd77b2
zyrouge <naveencnaveen96@gmail.com>
2022-02-13 11:57:25 +0530

    refactor: export fs utils

--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -1,6 +1,7 @@
 export './utils/duration.dart';
 export './utils/enum.dart';
 export './utils/error.dart';
+export './utils/fs.dart';
 export './utils/function.dart';
 export './utils/http.dart';
 export './utils/list.dart';
