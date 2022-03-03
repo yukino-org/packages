85ed36b900fe5244732c2934913bc73cc4c95bb9
zyrouge <naveencnaveen96@gmail.com>
2022-03-03 19:05:53 +0530

    chore: exports

--- lib/tenka.dart
+++ lib/tenka.dart
@@ -2,6 +2,7 @@ library tenka;
 
 export 'package:tenka_runtime/tenka_runtime.dart';
 export 'data/exports.dart';
+export 'extractor/exports.dart';
 export 'internals.dart';
 export 'metadata/exports.dart';
 export 'repository/exports.dart';
