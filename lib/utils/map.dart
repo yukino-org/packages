b498c79f406f02ae1f08ecc4328de4f60c7637c0
zyrouge <naveencnaveen96@gmail.com>
2022-03-20 14:21:00 +0530

    refactor: add maybeGet in map utils

--- lib/utils/map.dart
+++ lib/utils/map.dart
@@ -5,11 +5,40 @@ abstract class MapUtils {
   ) {
     dynamic local = data;
     for (final dynamic x in keys) {
-      try {
-        local = (local as Map<dynamic, dynamic>)[x];
-      } catch (err) {
-        throw Exception('Could not get key "$x" ($err)');
+      if (local is! Map<dynamic, dynamic>) {
+        throw Exception(
+          'Could not get key "$x" (Previous value was not a map)',
+        );
       }
+
+      if (!local.containsKey(x)) {
+        throw Exception(
+          'Could not get key "$x" (Previous value does not contain this key)',
+        );
+      }
+
+      local = local[x];
+    }
+    return local as T;
+  }
+
+  static T? maybeGet<T>(
+    final Map<dynamic, dynamic> data,
+    final List<dynamic> keys,
+  ) {
+    dynamic local = data;
+    for (final dynamic x in keys) {
+      if (local is! Map<dynamic, dynamic>) {
+        throw Exception(
+          'Could not get key "$x" (Previous value was not a map)',
+        );
+      }
+
+      if (!local.containsKey(x)) {
+        return null;
+      }
+
+      local = local[x];
     }
     return local as T;
   }
