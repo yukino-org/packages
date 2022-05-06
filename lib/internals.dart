ca4eea586be7d61496680086defce8e79154a809
zyrouge <naveencnaveen96@gmail.com>
2022-05-06 21:47:55 +0530

    chore: debug flag

--- lib/internals.dart
+++ lib/internals.dart
@@ -1,10 +1,14 @@
 import 'package:tenka_runtime/tenka_runtime.dart';
 
 abstract class TenkaInternals {
+  static bool isDebug = false;
+
   static Future<void> initialize({
     required final TenkaRuntimeOptions runtime,
+    final bool? isDebug,
   }) async {
     await TenkaRuntimeManager.initialize(runtime);
+    TenkaInternals.isDebug = isDebug ?? TenkaInternals.isDebug;
   }
 
   static Future<void> dispose() async {
