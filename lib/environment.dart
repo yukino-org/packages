9257ec4d7d42d3cfd2fb264c96c3f04d7717f5af
zyrouge <naveencnaveen96@gmail.com>
2022-06-24 16:54:08 +0530

    chore: update deps

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,16 +1,11 @@
 import 'package:tenka/tenka.dart';
-import 'package:utilx_desktop/webview/puppeteer/provider.dart';
 import 'utils/benchmark.dart';
 
 abstract class TenkaDevEnvironment {
   static Future<void> prepare() async {
     await TenkaInternals.initialize(
-      runtime: TenkaRuntimeOptions(
-        http: const TenkaRuntimeHttpClientOptions(ignoreSSLCertificate: true),
-        webview: WebviewManagerInitializeOptions(
-          PuppeteerProvider(),
-          const WebviewProviderOptions(),
-        ),
+      runtime: const TenkaRuntimeOptions(
+        http: TenkaRuntimeHttpClientOptions(ignoreSSLCertificate: true),
       ),
       isDebug: true,
     );
