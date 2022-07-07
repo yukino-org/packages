aff1ffb59573f64b696a9155548b094b42b09a11
zyrouge <naveencnaveen96@gmail.com>
2022-07-07 11:58:37 +0530

    refactor: path validators

--- lib/utils/fs.dart
+++ lib/utils/fs.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 abstract class FSUtils {
+  static final RegExp invalidPathReplacer = RegExp(r'[^\\\/\.\w- ]+');
+
   static Future<void> ensureDirectory(final Directory directory) async {
     if (!(await directory.exists())) {
       await directory.create(recursive: true);
@@ -28,4 +30,33 @@ abstract class FSUtils {
 
     await file.create(recursive: true);
   }
+
+  static String ensureWindowsPath(
+    final String path, [
+    final String replace = ' ',
+  ]) {
+    final String prefix =
+        RegExp('^[A-z]:').hasMatch(path) ? path.substring(0, 2) : '';
+
+    return prefix +
+        path.substring(prefix.length).replaceAll(invalidPathReplacer, replace);
+  }
+
+  static String ensureUnixPath(
+    final String path, [
+    final String replace = ' ',
+  ]) =>
+      path.replaceAll(invalidPathReplacer, replace);
+
+  static String ensurePath(
+    final String path, [
+    final String replace = ' ',
+  ]) {
+    if (Platform.isWindows) {
+      return ensureWindowsPath(path, replace);
+    }
+    return ensureUnixPath(path, replace);
+  }
+
+  static bool isValidPath(final String path) => path == ensurePath(path);
 }
