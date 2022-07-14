7b220a5357810413353f20c4563c02ea15e371a0
zyrouge <naveencnaveen96@gmail.com>
2022-07-14 17:21:51 +0530

    refactor: triple tuple

--- lib/utils/tuple.dart
+++ lib/utils/tuple.dart
@@ -4,3 +4,11 @@ class TwinTuple<A, B> {
   final A first;
   final B last;
 }
+
+class TripleTuple<A, B> {
+  const TripleTuple(this.first, this.middle, this.last);
+
+  final A first;
+  final A middle;
+  final B last;
+}
