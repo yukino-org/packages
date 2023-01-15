7e2411286c7ac771d09d8986f3daf2e378a0fc2e
Zyrouge <zyrouge@hotmail.com>
2023-01-15 15:47:26 +0530

    refactor: make html sync

--- lib/runtime/fubuki/bindings/url.dart
+++ lib/runtime/fubuki/bindings/url.dart
@@ -5,6 +5,15 @@ abstract class UrlBindings {
   static void bind(final FubukiNamespace namespace) {
     final FubukiObjectValue value = FubukiObjectValue();
     value.setNamedProperty(
+      'parse',
+      FubukiNativeFunctionValue.sync(
+        (final FubukiNativeFunctionCall call) {
+          final String url = call.argumentAt<FubukiStringValue>(0).value;
+          return newUrl(Uri.parse(url));
+        },
+      ),
+    );
+    value.setNamedProperty(
       'ensureHttpScheme',
       FubukiNativeFunctionValue.sync(
         (final FubukiNativeFunctionCall call) {
@@ -78,13 +87,10 @@ abstract class UrlBindings {
       'splitQueryString',
       FubukiNativeFunctionValue.sync(
         (final FubukiNativeFunctionCall call) {
-          final String query = call.argumentAt<FubukiStringValue>(0).value;
-          final FubukiObjectValue queries = FubukiObjectValue();
-          for (final MapEntry<String, String> x
-              in Uri.splitQueryString(query).entries) {
-            queries.setNamedProperty(x.key, FubukiStringValue(x.value));
-          }
-          return queries;
+          final FubukiStringValue value = call.argumentAt(0);
+          return convertQueryParametersToValue(
+            Uri.splitQueryString(value.value),
+          );
         },
       ),
     );
@@ -103,5 +109,116 @@ abstract class UrlBindings {
       ),
     );
     namespace.declare('Url', value);
+  }
+
+  static FubukiValue newUrl(final Uri url) {
+    final FubukiObjectValue value = FubukiObjectValue();
+    value.setNamedProperty(
+      'authority',
+      FubukiStringValue(url.authority),
+    );
+    value.setNamedProperty(
+      'fragment',
+      FubukiStringValue(url.fragment),
+    );
+    value.setNamedProperty(
+      'host',
+      FubukiStringValue(url.host),
+    );
+    value.setNamedProperty(
+      'origin',
+      FubukiStringValue(url.origin),
+    );
+    value.setNamedProperty(
+      'path',
+      FubukiStringValue(url.path),
+    );
+    value.setNamedProperty(
+      'port',
+      FubukiNumberValue(url.port.toDouble()),
+    );
+    value.setNamedProperty(
+      'query',
+      FubukiStringValue(url.query),
+    );
+    value.setNamedProperty(
+      'scheme',
+      FubukiStringValue(url.scheme),
+    );
+    value.setNamedProperty(
+      'hasAbsolutePath',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.hasAbsolutePath),
+      ),
+    );
+    value.setNamedProperty(
+      'hasAuthority',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.hasAuthority),
+      ),
+    );
+    value.setNamedProperty(
+      'hasEmptyPath',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.hasEmptyPath),
+      ),
+    );
+    value.setNamedProperty(
+      'hasFragment',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.hasFragment),
+      ),
+    );
+    value.setNamedProperty(
+      'hasPort',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.hasPort),
+      ),
+    );
+    value.setNamedProperty(
+      'hasQuery',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.hasQuery),
+      ),
+    );
+    value.setNamedProperty(
+      'hasScheme',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.hasScheme),
+      ),
+    );
+    value.setNamedProperty(
+      'isAbsolute',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiBooleanValue(url.isAbsolute),
+      ),
+    );
+    value.setNamedProperty(
+      'pathSegments',
+      FubukiNativeFunctionValue.sync(
+        (final _) => FubukiListValue(
+          url.pathSegments
+              .map((final String x) => FubukiStringValue(x))
+              .toList(),
+        ),
+      ),
+    );
+    value.setNamedProperty(
+      'queryParameters',
+      FubukiNativeFunctionValue.sync(
+        (final _) => convertQueryParametersToValue(url.queryParameters),
+      ),
+    );
+    return value;
+  }
+
+  static FubukiValue convertQueryParametersToValue(
+    final Map<String, String> queryParameters,
+  ) {
+    final FubukiObjectValue queries = FubukiObjectValue();
+    for (final MapEntry<String, String> x in queryParameters.entries) {
+      queries.setNamedProperty(x.key, FubukiStringValue(x.value));
+    }
+    return queries;
   }
 }
