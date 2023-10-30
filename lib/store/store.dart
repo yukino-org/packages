99668f89bbfcc63aaaf6d1c04c9447e22805cbc7
zyrouge <zyrouge@hotmail.com>
2023-10-30 19:11:00 +0530

    refactor: single base url

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -3,14 +3,13 @@ import '../metadata/exports.dart';
 
 class TenkaStore {
   const TenkaStore({
-    required this.baseURLs,
+    required this.baseURL,
     required this.modules,
     required this.builtAt,
-    required this.checksum,
   });
 
   factory TenkaStore.fromJson(final JsonMap json) => TenkaStore(
-        baseURLs: castJsonMap(json['baseURLs']),
+        baseURL: json['baseURL'] as String,
         modules: castJsonMap<String, JsonMap>(json['modules']).map(
           (final String i, final JsonMap x) => MapEntry<String, TenkaMetadata>(
             i,
@@ -18,24 +17,18 @@ class TenkaStore {
           ),
         ),
         builtAt: DateTime.parse(json['builtAt'] as String),
-        checksum: json['checksum'] as String,
       );
 
-  final Map<String, String> baseURLs;
+  final String baseURL;
   final Map<String, TenkaMetadata> modules;
   final DateTime builtAt;
-  final String checksum;
 
   JsonMap toJson() => <dynamic, dynamic>{
-        'baseURLs': baseURLs,
+        'baseURL': baseURL,
         'modules': modules.map(
           (final String i, final TenkaMetadata x) =>
               MapEntry<String, JsonMap>(i, x.toJson()),
         ),
         'builtAt': builtAt.toIso8601String(),
-        'checksum': checksum,
       };
-
-  static String generateChecksum() =>
-      StringUtils.toHex(StringUtils.random(inputLength: 20));
 }
