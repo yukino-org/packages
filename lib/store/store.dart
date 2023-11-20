df5c29d16c0ee11aa727d28bb1b3a2078b14743b
zyrouge <zyrouge@hotmail.com>
2023-11-20 20:39:53 +0530

    refactor: remove built at

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -5,23 +5,20 @@ class TenkaStore {
   const TenkaStore({
     required this.baseUrl,
     required this.modules,
-    required this.builtAt,
   });
 
   factory TenkaStore.fromJson(final JsonMap json) => TenkaStore(
         baseUrl: json['baseUrl'] as String,
         modules: castJsonMap<String, JsonMap>(json['modules']).map(
-          (final String i, final JsonMap x) => MapEntry<String, TenkaMetadata>(
-            i,
-            TenkaMetadata.fromJson(x),
+          (final String k, final JsonMap v) => MapEntry<String, TenkaMetadata>(
+            k,
+            TenkaMetadata.fromJson(v),
           ),
         ),
-        builtAt: DateTime.parse(json['builtAt'] as String),
       );
 
   final String baseUrl;
   final Map<String, TenkaMetadata> modules;
-  final DateTime builtAt;
 
   JsonMap toJson() => <dynamic, dynamic>{
         'baseUrl': baseUrl,
@@ -29,6 +26,5 @@ class TenkaStore {
           (final String i, final TenkaMetadata x) =>
               MapEntry<String, JsonMap>(i, x.toJson()),
         ),
-        'builtAt': builtAt.toIso8601String(),
       };
 }
